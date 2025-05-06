2fc6e7282b849d13004a2905270d15aede96dd2c
Urvan <urvan.christen@gmail.com>
2025-05-06 10:59:37 +0200

    CG-23 Create a fancy icon

--- app/src/main/java/com/example/coffeegeysha/MainActivity.kt
+++ app/src/main/java/com/example/coffeegeysha/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.coffeegeysha
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.coffeegeysha.databinding.ActivityMainBinding
-import timber.log.Timber
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
